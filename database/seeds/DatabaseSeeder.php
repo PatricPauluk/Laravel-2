b98ba2b8353332553822cf2f204b396d168837dc
Patric Pauluk <patric.pauluk@outlook.com>
2018-11-24 11:53:06 -0200

    aula 6

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use estoque\Categoria;
+
+/**
+ * Classe seeder, dedicada a preencher valores no banco de dados para testes, como produtos, usuários, etc.
+ * Normalmente não deve ser utilizada para o usuário final, apenas para ambientes de desenvolvimento.
+ * 
+ * Para executar um seeder, inserir no terminal:
+ * php artisan db:seed
+ * 
+ * Para executar um seeder (uma classe), em específico:
+ * php artisan db:seed --class=NomeTableSeeder
+ */
 
 class DatabaseSeeder extends Seeder
 {
@@ -10,7 +22,29 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
+    {   
+        /**
+         * Chama a classe que executa o seed para o banco de dados.
+         * $this->call(UsersTableSeeder::class);
+         */
+        $this->call('CategoriaTableSeeder');
+    }
+}
+
+/**
+ * É comum que a classe de seeder conste no mesmo arquivo como no exemplo abaixo, porém, caso o código seja muito
+ * extenso, é certo criar arquivos separados para organização.
+ */
+
+// Classe responsável pela inserção de valores no banco de dados.
+class CategoriaTableSeeder extends Seeder
+{
+    public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Insere um valor na coluna 'nome' da tabela 'categorias'.
+        Categoria::create(['nome' => 'Brinquedo']);
+        Categoria::create(['nome' => 'Esporte']);
+        Categoria::create(['nome' => 'Musica']);
+        Categoria::create(['nome' => 'Tecnologia']);
     }
 }
